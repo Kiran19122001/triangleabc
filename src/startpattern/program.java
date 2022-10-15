63ade5b7ec9fbf9448dbc7264d965c73566e70a3
Kiran19122001 <kiranban0011@gmail.com>
2022-10-15 11:41:15 +0530

    first commit

--- src/startpattern/program.java
+++ src/startpattern/program.java
@@ -1,23 +1,20 @@
 package startpattern;
+
 public class program {
-	public static void main(String[] arg) 
-	{
-		int n=5;
-		
-		for(int i=1;i<=n;i++)
-		{
-			char ch='A';
-			for(int j=1;j<=i;j++)
-			{
-				System.out.print(ch++ + " ");
-				
-			}System.out.println();
-				
-				
+	public static void main(String[] arg) {
+		int n = 5;
+		for (int i = 1; i <= n; i++) {
+			for (int k = 1; k <= n - i; k++) {
+				System.out.print(" ");
+			}
+			for (int j = 1; j <= i; j++) {
+				if (j == 1 || i == n || j == i) {
+					System.out.print("* ");
+				} else {
+					System.out.print("  ");
+				}
+			}
+			System.out.println();
 		}
-			
-				
 	}
-				
-}    		
-	
+}
